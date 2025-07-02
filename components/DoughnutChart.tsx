23090f5bffcd104ab0444540807430773e76d40c
pivnati <natanmuleta911@gmail.com>
2025-07-02 07:11:16 -0700

    modfiy the doughnutchart.tsx

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -8,30 +8,19 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
-  const accountNames = accounts.map((a) => a.name);
-  const balances = accounts.map((a) => a.currentBalance)
 
   const data = {
     datasets: [
       {
         label: 'Banks',
-        data: balances,
+        data: [1250,2500,3750],
         backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
       }
     ],
-    labels: accountNames
+    labels: ['Bank 1', 'Bank 2',]
   }
 
-  return <Doughnut 
-    data={data} 
-    options={{
-      cutout: '60%',
-      plugins: {
-        legend: {
-          display: false
-        }
-      }
-    }}
+  return <Doughnut data={data} 
   />
 }
 
