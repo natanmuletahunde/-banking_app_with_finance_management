2b773816e6054d6706253ef15b4660c67fa281a0
pivnati <natanmuleta911@gmail.com>
2025-07-04 13:05:22 -0700

    ad the router in the sign in and up form  of the auth ffuntioan

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -28,16 +28,45 @@ const AuthForm = ({ type }: { type: string }) => {
   })  
 
   // 2. Handle form submit
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-  
-    // Simulate a delay like an API call
-    setTimeout(() => {
-      console.log("Form submitted values:", values); // ✅ Now logs in browser
+
+    try {
+      // Sign up with Appwrite & create plaid token
+      
+      if(type === 'sign-up') {
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData);
+
+        setUser(newUser);
+      }
+
+      if(type === 'sign-in') {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+
+        if(response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
       setIsLoading(false);
-    }, 1000); // 1 second delay
+    }
   }
-  
   
 
   return (
